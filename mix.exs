8bf16482c2bc3aaf3b1e296254cc16d9c4339fd1
Matt Whitworth <matt@puretype.ai>
2024-03-14 16:39:49 +0000

    v0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTreeSitter.MixProject do
   def project do
     [
       app: :ex_tree_sitter,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       compilers: [:elixir_make] ++ Mix.compilers(),
